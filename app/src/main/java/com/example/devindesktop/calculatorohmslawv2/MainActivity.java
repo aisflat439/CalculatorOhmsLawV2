b1b0bdf9ce891d506aee0e700bb5a7af79f39c27
Devin P <devinfitzsimons@yahoo.com>
2016-07-16 15:03:43 -0400

    Added in Fragments and finished up basic Nav Drawer

--- app/src/main/java/com/example/devindesktop/calculatorohmslawv2/MainActivity.java
+++ app/src/main/java/com/example/devindesktop/calculatorohmslawv2/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.devindesktop.calculatorohmslawv2;
 
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private DrawerLayout mDrawer;
+    private NavigationView mNavDrawer;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mNavDrawer = (NavigationView) findViewById(R.id.nvView);
+
+        setupDrawerContent(mNavDrawer);
     }
 
     @Override
@@ -33,6 +43,50 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                selectDrawerItem(menuItem);
+                return true;
+            }
+        });
+
+    }
+
+    public void selectDrawerItem(MenuItem menuItem){
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()){
+            case R.id.navigation_framgent_one:
+                fragmentClass = FragmentOne.class;
+                break;
+            case R.id.navigation_framgent_two:
+                fragmentClass = FragmentTwo.class;
+                break;
+            case R.id.navigation_framgent_three:
+                fragmentClass = FragmentThree.class;
+                break;
+            default:
+                fragmentClass = FragmentOne.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frame_layout_content, fragment).commit();
+
+        menuItem.setChecked(true);
+
+        setTitle(menuItem.getTitle());
+
+        mDrawer.closeDrawers();
     }
 
     // onPostCreate called wehen activity startup is complete
